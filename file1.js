2134e82b71ee7aae37ec17de4bfe3ee85fd36723
Finahkaggwa <finahkaggwa@gmail.com>
2022-05-03 16:02:19 +0200

    new changes

---
+++ file1.js
@@ -0,0 +1,14 @@
+const names = ['andre', 'sino', 'sibo', 'nurha', 'simone']
+
+
+for (let counter = 0; counter < names.length; counter += 2) {
+ 	console.log( names[counter]) 
+  	
+}
+
+//  
+//..................................................//.......
+
+names.forEach(function(name){
+  	console.log("-->" + name)
+})
